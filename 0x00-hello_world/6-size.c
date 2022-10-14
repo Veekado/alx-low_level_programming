6c7c402b8ff1cd9ff12eb3988446d55389c51eed
Imeh Victor <imohimeh33@gmail.com>
2022-10-13 19:40:30 -0700

    size32 and size64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* 
+/**
  * main - Entry point
  *
  * Return: Always 0(Success)
@@ -10,7 +10,7 @@
 	printf("size of a char: %lu bytes \n", sizeof(char));
 	printf("size of an int: %lu bytes \n", sizeof(int));
 	printf("size of a long: %lu bytes \n", sizeof(long));
-	printf("size of a long long: %lu bytes \n", sizeof (long long));
+	printf("size of a long long: %lu bytes \n", sizeof(long long));
 	printf("size of a float: %lu bytes \n", sizeof(float));
-	return(0);
+	return (0);
 }
