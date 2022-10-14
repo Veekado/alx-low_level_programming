974f86c578ca2838b3d303e504c9d4f89bb0528c
Imeh Victor <imohimeh33@gmail.com>
2022-10-13 19:24:10 -0700

    mandatory task done.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* 
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ *
+ */
+	int main(void)
+{
+	printf("size of a char: %lu bytes \n", sizeof(char));
+	printf("size of an int: %lu bytes \n", sizeof(int));
+	printf("size of a long: %lu bytes \n", sizeof(long));
+	printf("size of a long long: %lu bytes \n", sizeof (long long));
+	printf("size of a float: %lu bytes \n", sizeof(float));
+	return(0);
+}
