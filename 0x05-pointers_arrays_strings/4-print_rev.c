24c8794835384435cfad71756a3d913e7246e019
Imeh Victor <imohimeh33@gmail.com>
2022-10-24 16:03:30 -0700

    made a correction and added a script

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: a pointer to the str to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+
+	i = len - 1;
+
+	while (i >= 0)
+		i--;
+
+		_putchar(s[i]);
+	_putchar('\n');
+}
