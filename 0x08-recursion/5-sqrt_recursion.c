57a0dbccbcfe145c2a0d2af6056df2d94efb1252
Imeh Victor <imohimeh33@gmail.com>
2022-11-03 18:19:33 -0700

    last mandatory

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
-int sqroot(int j, int k);
+int find_sqrt(int n, int i);
 
 /**
  * _sqrt_recursion - finds the square root of a number
  * @n: the number
  *
- * Return: an integer(natural square root or -1)
+ * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
 {
-	return (sqroot(n, 0));
+	return (find_sqrt(n, 0));
 }
 
 /**
- * sqroot - finds the square root of a number
- * @j:a number
- * @k: value obtained
+ * find_sqrt - finds the square root of a number
+ * @n: the number
+ * @i: find
  *
  * Return: natural square root or -1
  */
-int sqroot(int j, int k)
+int find_sqrt(int n, int i)
 {
-	if (j * j == k)
-		return (j);
-	else if (j * j > k)
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
 		return (-1);
 	else
-		return (sqroot(k, j + 1));
+		return (find_sqrt(n, i + 1));
 }
