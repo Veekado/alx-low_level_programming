dc88920a1ad815d30f0a1b5385c0e010cc35d2fa
Imeh Victor <imohimeh33@gmail.com>
2022-11-10 14:34:46 -0800

    new new

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,22 +9,22 @@
  */
 int *array_range(int min, int max)
 {
-	int *arr, i, n = 0, initial = min;
+	int *arr, i, len = 0, start = min;
 
 	if (min > max)
 		return (NULL);
 
-	while (initial <= max)
+	while (start <= max)
 	{
-		n++;
-		initial++;
+		len++;
+		start++;
 	}
 
-	arr = malloc(n * sizeof(int));
+	arr = malloc(len * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < len; i++)
 
 	{
 		arr[i] = min;
