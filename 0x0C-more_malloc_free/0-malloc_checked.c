feb644897855c94900fe78a47dc1a5b8b4ccfad8
Imeh Victor <imohimeh33@gmail.com>
2022-11-10 14:27:35 -0800

    new

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
 *malloc_checked - a function that allocates memory using malloc
@@ -16,5 +17,6 @@ void *malloc_checked(unsigned int b)
 
 	if (s == NULL)
 		exit(98);
+
 	return (s);
 }
