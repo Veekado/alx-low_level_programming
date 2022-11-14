42eb91a919d20f8e8e0732ec61bfe737dfdeda21
Imeh Victor <imohimeh33@gmail.com>
2022-11-14 13:50:53 -0800

    macro n preprocessors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - prints the name of the file it was compiled from,followed by a new line
+*Return: returns 0
+*/
+
+int main(void)
+{
+		printf("The name of the program gile is: %s\n", _FILE_);
+		return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+a look at C preprocessors, object-like macro and function-like macro
