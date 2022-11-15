c00980fe8f35ef3ab924c1d575d56f3838e6a85b
Imeh Victor <imohimeh33@gmail.com>
2022-11-15 15:51:13 -0800

    struct error fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,47 +1,50 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Return: returns a pointer to newly created dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: the new dog greated or null if failed
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	int name_len, owner_len, i;
+	dog_t *dog;
 
-	if (name == NULL || owner == NULL)
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL || !name || !owner)
+	{
+		free(dog);
 		return (NULL);
+	}
 
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
+	name_len = strlen(name);
+	owner_len = strlen(owner);
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	dog->name = malloc(name_len + 1);
+	dog->owner = malloc(owner_len + 1);
+
+	if (!(dog->name) || !(dog->owner))
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+
 		return (NULL);
+	}
 
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
-		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
+	for (i = 0; i < name_len; i++)
+		dog->name[i] = name[i];
+	dog->name[i] = '\0';
 
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
+	dog->age = age;
 
-	new_dog->name = cpyname;
-	new_dog->age = age;
-	new_dog->owner = cpyowner;
-	return (new_dog);
+	for (i = 0; i < owner_len; i++)
+		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
+
+	return (dog);
 }
